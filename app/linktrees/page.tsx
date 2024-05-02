af58669f68d6c435958844e8170583224a23b88f
besta <alfidibesta@gmail.com>
2024-05-02 16:21:47 +0700

    merapikan ajah

--- app/linktrees/page.tsx
+++ app/linktrees/page.tsx
@@ -1,7 +1,10 @@
 const Linktrees = () => {
-    return (
-        <h1>PAGE LINKTREES</h1>
-    )
-}
+  return (
+    <>
+      <h1>PAGE LINKTREES</h1>
+      <p>MASIH BELOM DI BUAT!</p>
+    </>
+  );
+};
 
 export default Linktrees;
--- app/page.tsx
+++ app/page.tsx
@@ -3,30 +3,36 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="main-page">
-      <h1>Halaman utama belajar next js v1</h1>
-
-      <br />
-      <Link
-        href="/latians"
-        target="_blank"
-        className="w-full bg-[tomato] text-white m-5 rounded-md p-2"
-      >
-        - latianssssz
-      </Link>
-      <Link
-        href="/linktrees"
-        target="_blank"
-        className="w-full bg-[tomato] text-white m-5 rounded-md p-2"
-      >
-        - mau bikin linktree abal abal :)
-      </Link>
-      <Link
-        href="https://alfidi.vercel.app/"
-        target="_blank"
-        className="w-full bg-[tomato] text-white m-5 rounded-md p-2"
-      >
-        - web vercel gw 😁
-      </Link>
+      <h1 className="font-bold">HALAMAN UTAMA LATIAN :D</h1>
+      <ul className="mt-5">
+        <li className="main-list">
+          <Link
+            href="/latians"
+            target="_blank"
+            className="w-full bg-[tomato] text-white m-5 rounded-md p-2 hover:bg-blue-600"
+          >
+            - latianssssz next js v1
+          </Link>
+        </li>
+        <li className="main-list">
+          <Link
+            href="/linktrees"
+            target="_blank"
+            className="w-full bg-[tomato] text-white m-5 rounded-md p-2 hover:bg-blue-600"
+          >
+            - mau bikin linktree abal abal :)
+          </Link>
+        </li>
+        <li className="main-list">
+          <Link
+            href="https://alfidi.vercel.app/"
+            target="_blank"
+            className="w-full bg-[tomato] text-white m-5 rounded-md p-2 hover:bg-blue-600"
+          >
+            - CV *build with astro 😁
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
