51894c39879b33d84a7664daed9d1810985a5646
besta <alfidibesta@gmail.com>
2024-05-03 16:34:59 +0700

    add link git

--- app/data/dataList.tsx
+++ app/data/dataList.tsx
@@ -3,21 +3,25 @@ const dataList = [
       title: "Latian Next.js v1",
       link: "/pages/latians",
       desc: "Belajar menggunakan Next.js v1",
+      git: "https://github.com/alfidibesta/latihan-nextjs",
     },
     {
       title: "Linkthree kw",
       link: "/pages/linktrees",
       desc: "Mau bikin linktree kw 😙",
+      git: "",
     },
     {
       title: "CV v2",
       link: "https://alfidi.vercel.app/",
       desc: "Bikin sendiri pake astro, ini agak menyala temanya",
+      git: "https://github.com/alfidibesta/alfidi",
     },
     {
       title: "CV v1",
       link: "https://alfidibesta.github.io/",
       desc: "Ini ambil template orang",
+      git: "https://github.com/alfidibesta/alfidibesta.github.io",
     },
   ];
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
-import { CircleArrowRight } from "lucide-react";
+import { CircleArrowRight, Github } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import dataList from "./data/dataList";
-
 
 export default function Home() {
   return (
@@ -15,22 +14,44 @@ export default function Home() {
           <Separator className="my-2" />
           <div className="mt-5">
             {dataList.map((data, index) => (
-              <Link
-                href={data.link}
-                target="_blank"
-                className="link"
-                key={index}
-              >
-                <Alert className="list-content">
-                  <CircleArrowRight
-                    size={16}
-                    strokeWidth={2.5}
-                    className="mr-4 circle-arrow"
-                  />
-                  <AlertTitle>{data.title}</AlertTitle>
-                  <AlertDescription>{data.desc}</AlertDescription>
-                </Alert>
-              </Link>
+              <div className="flex" key={index}>
+                <div className="w-full mr-5">
+                  <Link
+                    href={data.link}
+                    target="_blank"
+                    className="link"
+                    key={index}
+                  >
+                    <Alert className="list-content relative">
+                      <CircleArrowRight
+                        size={16}
+                        strokeWidth={2.5}
+                        className="mr-4 circle-arrow"
+                      />
+                      <AlertTitle className="">
+                        <span>{data.title}</span>
+                      </AlertTitle>
+                      <AlertDescription>{data.desc}</AlertDescription>
+                    </Alert>
+                  </Link>
+                </div>
+                <div className="w-auto flex items-center justify-end relative">
+                  <a href={data.git} className="relative block">
+                    <Github
+                      size={16}
+                      strokeWidth={2.5}
+                      className="transform -translate-y-1/2"
+                    />
+                    <div className="transform -translate-y-1/2 absolute top-0 -right-2 w-8 h-8 rounded-full bg-black transition-all duration-300 opacity-0 hover:opacity-100 flex items-center justify-center">
+                      <Github
+                        size={16}
+                        strokeWidth={2.5}
+                        className="text-green-500"
+                      />
+                    </div>
+                  </a>
+                </div>
+              </div>
             ))}
           </div>
         </div>
